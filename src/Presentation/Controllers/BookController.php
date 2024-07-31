db398f4eb046851419d6b1cb839960cd235ccbab
Filip Kojic <filip.kojic@logeecom.com>
2024-07-31 13:53:50 +0200

    Delete sufficient pages
    Delete addBook and deleteBook
    
    ISSUE: MIDU-850

--- src/Presentation/Controllers/BookController.php
+++ src/Presentation/Controllers/BookController.php
@@ -61,54 +61,6 @@ class BookController {
         return $response;
     }
 
-    /**
-     * Create a new book.
-     *
-     * @param HttpRequest $request
-     * @return HttpResponse
-     */
-    public function create(HttpRequest $request): HttpResponse {
-        $nameError = $yearError = "";
-        $name = $year = "";
-        $authorId = intval($request->getBodyParam('authorId', null));
-
-        if ($request->getMethod() == "POST" && $request->getBodyParam('formSubmitted')) {
-            $name = $request->getBodyParam("name");
-            $year = $request->getBodyParam("year");
-
-            if (empty($name)) {
-                $nameError = "* This field is required";
-            } elseif (strlen($name) > 250) {
-                $nameError = "* Name must be less than 250 characters";
-            } else {
-                $name = htmlspecialchars($name);
-            }
-
-            if (empty($year)) {
-                $yearError = "* This field is required";
-            } elseif (!is_numeric($year) || $year == 0 || $year < -5000 || $year > 999999) {
-                $yearError = "* Year must be a number between -5000 and 999999 (0 is invalid)";
-            } else {
-                $year = intval($year);
-            }
-
-            if (empty($nameError) && empty($yearError)) {
-                $this->bookService->addBook($name, $year, $authorId);
-                $response = new HttpResponse();
-                $response->setStatusCode(302);
-                $response->addHeader('Location', '/books/' . $authorId);
-                return $response;
-            }
-        }
-
-        ob_start();
-        include __DIR__ . '/../Views/addBook.php';
-        $content = ob_get_clean();
-
-        $response = new HttpResponse();
-        $response->setBody($content);
-        return $response;
-    }
 
     /**
      * Edit an existing book.
@@ -162,35 +114,7 @@ class BookController {
         return $response;
     }
 
-    /**
-     * Delete a book.
-     *
-     * @param HttpRequest $request
-     * @param int $id Book ID.
-     * @return HttpResponse
-     */
-    public function delete(HttpRequest $request, int $id): HttpResponse {
-        $book = $this->bookService->getBookById($id);
-        $authorId = $book->getAuthorId();
-
-        if ($request->getMethod() == "POST") {
-            $this->bookService->deleteBook($id);
-            $response = new HttpResponse();
-            $response->setStatusCode(302);
-            $response->addHeader('Location', '/books/' . $authorId);
-            return $response;
-        }
-
-        ob_start();
-        include __DIR__ . '/../Views/deleteBook.php';
-        $content = ob_get_clean();
-
-        $response = new HttpResponse();
-        $response->setBody($content);
-        return $response;
-    }
-
-    // ajax metode
+        // ajax metode
     /**
      * Get a list of books by author ID as a JSON response.
      *
